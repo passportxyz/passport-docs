c439d6d3005aee8a7cae017338c580af11179271
lebraat <dan.brunsdon@gmail.com>
2023-08-27 13:47:49 -0600

    Added head with GA tag and social metadata

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,6 @@
 import { DocsThemeConfig } from 'nextra-theme-docs'
 import { passportLogo } from './components/logo'
+import NextScript from "next/script";
 
 const config: DocsThemeConfig = {
   logo: passportLogo,
@@ -14,6 +15,35 @@ const config: DocsThemeConfig = {
     text: 'Gitcoin Passport docs',
   },
   faviconGlyph: "public/favicon.png",
-}
+};
 
-export default config
+const CustomHead: React.FC = () => (
+  <>
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@gitcoinpassport" />
+    <meta name="twitter:image" content="https://docs.passport.gitcoin.co/social-card.png" />
+    <meta name="og:title" content="Gitcoin Passport"  />
+    <meta name="og:description" content="Gitcoin Passport — Sybil Defense. Made Simple."  />
+    <meta name="og:image" content="https://docs.passport.gitcoin.co/social-card.png" />
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-EDEYF2MWC5"></script>
+    <NextScript>
+    {/* Add the Google Analytics script */}
+    <script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=G-EDEYF2MWC5"
+    ></script>
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-EDEYF2MWC5');
+        `,
+      }}
+    ></script>
+  </NextScript>
+  </>
+);
+
+export default { ...config, head: CustomHead };
